a03873e56ad9a6fa109e33dc0f5cc2680dbecb59
Sophie Gu <sophieqgu@gmail.com>
2018-10-11 14:11:05 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(array)
+  array.sort 
+end 
+  
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a  
+  end 
+end 
 
+def sort_array_char_count(array)
+  array.sort do |x, y|
+    x.length <=> y.length 
+  end 
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array 
+end 
+    
+def reverse_array(array)
+  array.reverse 
+end 
+
+def kesha_maker(array)
+  array.collect do |string| 
+    string[2] = "$"
+    string 
+  end 
+end 
+    
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end 
+end
+
+def sum_array(array)
+  array.inject do |sum, int|
+    sum += int
+  end 
+end 
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    word << "s" if index != 1
+  end 
+  array
+end 
+  
